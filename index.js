2893acf9a45974a647b850573d200cec6aa7ae75
OLUWABORIOV <odeyaleoluwabori@gmail.com>
2022-09-03 23:21:28 -1200

    commit

--- index.js
+++ index.js
@@ -90,13 +90,18 @@ logLapTime()*/
 // change the count-el in the HTML to reflect the new count
 
 let countEL = document.getElementById("count-el")
-console.log(countEL)
 
 let count = 0
 
 function increment() {
     count = count + 1
+    countEL.innerText = count
+}
 
+// 1. Create a function, save(), which logs out the count when it's called
+
+function save() {
     console.log(count)
 }
+
 
