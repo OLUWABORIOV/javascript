f2ba34054761fb526959c13c430b20463f3c3d35
OLUWABORIOV <odeyaleoluwabori@gmail.com>
2022-09-16 22:52:43 -1200

    commit

--- index.js
+++ index.js
@@ -151,7 +151,33 @@ logLapTime()*/
 //   console.log(greeting + ", " + name + "!")
 //}
 //grt()
-function purchase() {
-   console.log(button clicked)
-}
 
+//rParagraph = document.getElementById("error")
+//console.log("errorParagraph")
+
+//function purchase() {
+//   console.log("button clicked")
+//   errorParagraph.textContent = "Something went wrong, please try again"
+//}
+let num1 = 8
+let num2 = 2
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+let sumEl = document.getElementById("sum-el")
+
+function add() {
+   let result = num1 + num2
+   sumEl.textContent =  "Sum: " + result
+}
+function subtract() {
+   let result = num1 - num2
+   sumEl.textContent =  "Sum: " + result
+}
+function divide() {
+   let result = num1 / num2
+   sumEl.textContent =  "Sum: " + result
+}
+function multiplication() {
+   let result = num1 * num2
+   sumEl.textContent =  "Sum: " + result
+}
