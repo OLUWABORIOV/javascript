c246285dc654851ac4c350cf445f40235a099465
OLUWABORIOV <odeyaleoluwabori@gmail.com>
2022-11-05 11:10:21 -1200

    commit

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
-let count = 5 + 7
+//let count = 5 + 7
 
-console.log(count)
+// //console.log(count)
+// let myAge = 22
+// let humanDogRatio = 4
+// let myDogAge = myAge * humanDogRatio
+
+// console.log(myDogAge)
+
+// let bonusPoints = 50
+
+// bonusPoints = bonusPoints
+
+// bonusPoints = bonusPoints - 25
+
+// bonusPointsn = bonusPoints + 45
+
+// console.log(bonusPoint)
+// function increment() {
+    // console.log("the button was clicked")
+// }
+function countdown() {
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
+}
+
+countdown()
+
+countdown()
+
+countdown()
+
+countdown()
