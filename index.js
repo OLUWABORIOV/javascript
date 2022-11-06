8f290ca91b17922213fd5ab29b8ca826f782ff84
OLUWABORIOV <odeyaleoluwabori@gmail.com>
2022-11-05 21:38:48 -1200

    c

--- index.js
+++ index.js
@@ -19,18 +19,60 @@
 // function increment() {
     // console.log("the button was clicked")
 // }
-function countdown() {
-    console.log(5)
-    console.log(4)
-    console.log(3)
-    console.log(2)
-    console.log(1)
-}
+// function countdown() {
+    // console.log(5)
+    // console.log(4)
+    // console.log(3)
+    // console.log(2)
+    // console.log(1)
+// }
 
-countdown()
+// countdown()
 
-countdown()
+// countdown()
 
-countdown()
+// countdown()
 
-countdown()
+// countdown()
+
+// function bori() {
+    // console.log(42)
+// }
+// bori()
+// let lap1 = 34
+// let lap2 = 33 
+// let lap3 = 36
+
+// function bori() {
+    // console.log(lap1 + lap2 + lap3)
+// }
+
+// bori()
+
+// let lapsCompleted = 0
+
+// function incrementlap() {
+//     lapsCompleted = lapsCompleted + 1
+// }
+//     incrementlap()
+//     incrementlap()
+//     incrementlap()
+
+// console.log(lapsCompleted)
+
+// let count = 0
+
+// function increment() {
+//     count = count + 1
+//     console.log(count)
+// }
+
+
+
+
+
+
+
+
+
+
