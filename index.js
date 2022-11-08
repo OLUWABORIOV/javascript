657b433f867f8ffe512b599ecb4f50c2b3da8950
OLUWABORIOV <odeyaleoluwabori@gmail.com>
2022-11-07 21:58:49 -1200

    co

--- index.js
+++ index.js
@@ -88,12 +88,35 @@
 // }
 
 // let count = 0
-// let countEl = document.getElementById("count-el")
+// 
 
 // function increment() {
 //     count = count + 1
 //     countEl.innerText = count
 // }
+
+
+function save() {
+    console.log(count)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
